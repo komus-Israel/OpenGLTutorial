b22fa8c6a1afbd8ecf3f64658932109457f683e2
komus-Israel <komolehinisrael@gmail.com>
2024-05-04 16:07:58 +0000

    Created a method to handle compilation of shaders. Refactored compileVertexShader method to use this new method for compilation

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -2,26 +2,69 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-
+//  The graphics pipeline converts a set of 3D co-ordinates into
+//  2D pixels that fits in the screen
+//  First, 3D co-ordinates are converted to 2D co-ordinates
+//  Secondly, the 2D co-ordinates are converted into colored pixels
 class GraphicsPipeline {
 
-    //  vertex shader
+    //  Vertex Shader
     const char *vertexShaderSource = "#version 330 core\n"
         "layout (location=0) in vec3 aPos;\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         "}\0";
     ;
 
-    //  The graphics pipeline converts a set of 3D co-ordinates into
-    //  2D pixels that fits in the screen
-    //  First, 3D co-ordinates are converted to 2D co-ordinates
-    //  Secondly, the 2D co-ordinates are converted into colored pixels
+    //  Fragment Shader
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+    ;
+
+private:
+
+     void compileShader(const char *shaderSource, unsigned int shaderType) {
+       
+        //  In order for opengl to use the shader, it has to dynamically compile it at run-time from its
+        //  source code
+        //  the type of shader being created is passed to function below (vertex, fragment, etc)
+        unsigned int shader = glCreateShader(shaderType);
+        
+        // Attach the shader source code to the shader object and compile the shader
+        glShaderSource(shader, 1, &shaderSource, NULL);
+        glCompileShader(shader);
+
+        int success;
+        char infoLog[512];
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+        if (!success) {
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cout << infoLog << std::endl;
+
+            if (shaderType == GL_VERTEX_SHADER) {
+
+                throw std::runtime_error("Vertex Shader compilation failed");
+
+            } else if (shaderType == GL_FRAGMENT_SHADER) {
+
+                throw std::runtime_error("Fragment Shader compilation failed");
+
+            }
+            
+        }
+    }   
+
 
 public:
     unsigned int VBO;
     unsigned int vertexShader;
+    unsigned int fragmentShader;
 
     //  vertices data for the triangle
     float vertices[9] = {
@@ -60,24 +103,18 @@ public:
         //  This is done by creating memory on the GPU where we store the vertex data
         //  This memory is handled by the VBO
 
-        //  In order for opengl to use the shader, it has to dynamically compile it at run-time from its
-        //  source code
-        //  since a vertex shader is being created, it is passed as a shader type to the function below
-        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        //  compile vertex shader
+        compileShader(vertexShaderSource, GL_VERTEX_SHADER);
 
-        //  Next attach the shader source code to the shader object and compile the shader
-        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-        glCompileShader(vertexShader);
+    }
 
-        int success;
-        char infoLog[512];
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    //  Fragment shader is about calculating the color output of the pixels
+    //  Colors are represented as an array of 4 values (RGBA)
+    //  The strenth of each component is a value between 0.0 and 1.0
+    void compileFragmentShader() {
 
-        if (!success) {
-            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            std::cout << infoLog << std::endl;
-            throw std::runtime_error("Vertex Shader compilation failed");
-        }
+       //   compile fragment shader
+       compileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
 
     }
 
