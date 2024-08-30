b446f8dec63b7d56b2d5e33614f04ba9a2416b30
komus-Israel <komolehinisrael@gmail.com>
2024-08-30 17:09:40 +0000

    Updated the vertex and fragment shader

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -12,20 +12,23 @@ class GraphicsPipeline {
 
     //  Vertex Shader
     const char *vertexShaderSource = "#version 330 core\n"
-        "layout (location=0) in vec3 aPos;\n"
+        "layout (location=0) in vec3 aPos;\n"   //  the position variable has attribute position 0
+        "layout (location=1) in vec3 aColor;\n" //  the color variable has attribute position 1
+        "out vec3 ourColor;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   ourColor = aColor;\n"
         "}\0";
     ;
 
     //  Fragment Shader
     const char *fragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 ourColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = ourColor;\n"
+        "   FragColor = vec3(ourColor, 1.0);\n"
         "}\0";
     ;
 
